624c092ce3917760744a821729a1d4ea115f46fb
Colin Charman <ccharm480@gmail.com>
2021-09-11 23:34:26 -0500

    minor update

--- vpc.tf
+++ vpc.tf
@@ -3,20 +3,15 @@
 resource "aws_vpc" "main" {
   cidr_block       = "192.168.200.0/24"
   instance_tenancy = "default"
-
   enable_dns_support = true
-
   enable_dns_hostnames = true
-
   enable_classiclink = false
-
   enable_classiclink_dns_support = false
-
   assign_generated_ipv6_cidr_block = false
 
   # optional
   tags = {
-    Name = "main"
+    Name = locals.tags
   }
 }
 
